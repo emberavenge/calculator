4793f9b8e1c272ee0760061835062bdf643d9c49
Emerson Castillo <emberavenge@protonmail.com>
2023-11-29 16:50:28 -0400

    Fixed add function not properly functioning

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function add(num1, num2) {
-    return num1, num2;
+    return num1 + num2;
 }
 
 function subtract(num1, num2) {
@@ -17,3 +17,4 @@ function divide (num1, num2) {
 let firstNumber = 3;
 let operators = ['+', '-', '*','/'];
 let secondNumber = 5;
+
