4b6978f36d3c1456c1795d30171e86a2ab94e733
Emerson Castillo <emberavenge@protonmail.com>
2023-11-29 16:55:21 -0400

    Added the operate function

--- app.js
+++ app.js
@@ -10,11 +10,22 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide (num1, num2) {
+function divide(num1, num2) {
     return num1 / num2;
 }
 
 let firstNumber = 3;
-let operators = ['+', '-', '*','/'];
+let operators = ['+', '-', '*', '/'];
 let secondNumber = 5;
 
+function operate(operator, firstNum, secNum) {
+    if (operator == '+') {
+        return add(firstNum, secNum);
+    } else if (operator == '-') {
+        return subtract(firstNum, secNum)
+    } else if (operator == '*') {
+        return multiply(firstNum, secNum)
+    } else if (operator == '/') {
+        return divide(firstNum, secNum)
+    }
+}
